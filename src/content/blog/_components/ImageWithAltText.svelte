324746858083427b50b18ee07f47802c8cd7e088
Vivi <vivi.the@enbywit.ch>
2023-07-10 15:47:24 -0700

    writing

--- src/content/blog/_components/ImageWithAltText.svelte
+++ src/content/blog/_components/ImageWithAltText.svelte
@@ -1,8 +1,15 @@
 <script>
   export let alt;
+
+  $: splitText = alt.split('|')
+
+  $: console.log(splitText)
+
+  $: imgAltDescription = splitText.length > 1 ? splitText[1] : alt
+  $: figCaption = splitText.length > 1 ? splitText[0] : alt
 </script>
 
 <figure class="flex flex-col w-full justify-center items-center">
-  <img {alt} {...$$props} />
-  <figcaption class="text-center">{alt}</figcaption>
+  <img {...$$props} alt={imgAltDescription} />
+  <figcaption class="text-center">{figCaption}</figcaption>
 </figure>
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,25 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  console.log(import.meta.env.DEV);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts
+      .filter((v) => {
+        // if DEV, show draft posts
+        if (import.meta.env.DEV == true) {
+          return true;
+        }
+        return v.data.draft !== true;
+      })
+      .map((post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      })),
+  });
 }
