ca7966a1dc6ee2e827555e44b07bbdbf65bef153
Vivi <vivi.the@enbywit.ch>
2023-07-10 22:52:50 -0700

    finish the chost port

---
+++ src/content/blog/_components/Collapsible.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+  import { slide } from "svelte/transition";
+
+  let loaded = false;
+
+  onMount(() => {
+    loaded = true;
+  });
+
+  let visible = false;
+</script>
+
+{#if loaded}
+  <div
+    class="m-0 p-0 flex flex-col dark:bg-gray-800/25 border border-black/25 rounded-md"
+  >
+    <button on:click={() => (visible = !visible)}
+      >{visible ? "Hide" : "Show"}</button
+    >
+    {#if visible}
+      <div class="m-0 p-4" transition:slide|global>
+        <slot />
+      </div>
+    {/if}
+  </div>
+{:else}
+  <details>
+    <summary />
+    <slot />
+  </details>
+{/if}
